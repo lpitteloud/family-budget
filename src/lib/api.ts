82481cb9d87a15ee55b0d7756ae5642a0872cd61
Laurent Pitteloud <l.pitteloud@gmail.com>
2024-11-26 09:36:00 +0100

    remove useless spent property when creating a category

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -17,7 +17,6 @@ export const createCategory = async (name: string, budget: number) => {
     .insert([{ 
       name, 
       budget,
-      spent: 0,
       color: `#${Math.floor(Math.random()*16777215).toString(16)}`
     }])
     .select()
